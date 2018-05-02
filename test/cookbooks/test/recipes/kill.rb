d6ee25009c4c335f16b08463e977e01504537511
Tor Magnus Rakvåg <tm@intility.no>
2018-05-02 12:15:17 +0200

    use stable instead of current for testing
    Signed-off-by: Tor Magnus Rakvåg <tm@intility.no>

--- test/cookbooks/test/recipes/kill.rb
+++ test/cookbooks/test/recipes/kill.rb
@@ -1,5 +1,5 @@
 chef_client_updater "Install Chef #{node['chef_client_updater']['version']}" do
-  channel 'current'
+  channel 'stable'
   version node['chef_client_updater']['version']
   post_install_action 'kill'
 end
