913297ca4610542aa46f57a774dd0f4ca96ca48f
Joshua Justice <jjustice6@bloomberg.net>
2018-07-31 15:05:22 -0400

    Add install_timeout to one of the tests so there's a test for it.
    Signed-off-by: Joshua Justice <jjustice6@bloomberg.net>

--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -2,4 +2,5 @@ chef_client_updater "Install Chef #{node['chef_client_updater']['version']}" do
   channel 'stable'
   version node['chef_client_updater']['version']
   post_install_action 'exec'
+  install_timeout 599
 end
