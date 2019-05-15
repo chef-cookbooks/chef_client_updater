adb26b04ab75e68e1377fe7cbf9d5fdb68d4c73a
Tim Smith <tsmith@chef.io>
2019-05-14 22:13:32 -0700

    Use new ChefSpec format
    Modernize the specs a bit
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
-describe 'default recipe on ubuntu 16.04' do
-  let(:runner) { ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04') }
-  let(:chef_run) { runner.converge('test::default') }
+describe 'test::default' do
+  platform 'ubuntu', '16.04'
 
   it 'converges successfully' do
     expect { :chef_run }.to_not raise_error
