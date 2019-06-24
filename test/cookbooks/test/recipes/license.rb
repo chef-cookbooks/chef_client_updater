95067e54dcaa91eed4f8fd8245b8467ddc0854ed
tyler-ball <tball@chef.io>
2019-06-24 14:57:26 -0600

    Chef 15 released, pull that from stable channel for tests
    Signed-off-by: tyler-ball <tball@chef.io>

--- test/cookbooks/test/recipes/license.rb
+++ test/cookbooks/test/recipes/license.rb
@@ -1,8 +1,7 @@
 node.override['chef_client']['chef_license'] = 'accept'
 
 chef_client_updater 'Install Chef 15' do
-  # TODO: stable once this is released
-  channel 'current'
+  channel 'stable'
   version '15'
   post_install_action 'exec'
   install_timeout 599
