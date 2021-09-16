75cd375af238c5490c90fd75161c40747ead12f9
ayushbhatt29 <abhatt@msystechnologies.com>
2021-09-16 14:42:56 +0530

    Test PR (Do not MERGE)
    Signed-off-by: ayushbhatt29 <abhatt@msystechnologies.com>

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'test::default' do
-  platform 'ubuntu', '16.04'
+  platform 'ubuntu', '18.04'
 
   it 'converges successfully' do
     expect { :chef_run }.to_not raise_error
